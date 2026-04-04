bbdd9bf99aebaa926acba3f7cb771b926551feff
Thomas Schlott <thomas.schlott@outlook.de>
2026-04-04 19:38:10 +0200

    Updated google test

--- src/action/handler.cpp
+++ src/action/handler.cpp
@@ -4,4 +4,6 @@ namespace Terrahertz::Action {
 
 Handler::Handler(ImageProcessing::INode<BGRAPixel> *const imageInput, Input::Emulator *const emulator) noexcept {}
 
+bool Handler::next() noexcept {return false;}
+
 } // namespace Terrahertz::Action
